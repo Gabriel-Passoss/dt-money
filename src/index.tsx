a90c5569c4a4a55b3c798e8e4b1e04c0f9997d5e
Gabriel Passos <kooujinproductions@gmail.com>
2022-04-12 06:50:34 -0300

    Added Modal and formatting for data

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
-ReactDOM.render(
+createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 09:00:00'),
+        }
+      ] 
+    })
+  },
+  
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement!)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
