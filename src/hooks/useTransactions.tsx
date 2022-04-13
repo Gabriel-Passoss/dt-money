051b309da9393469300e3b88ce931206522216bb
Gabriel Passos <kooujinproductions@gmail.com>
2022-04-13 00:34:32 -0300

    Created a custom hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react'
 import { api } from '../services/api'
 
 interface Transaction {
@@ -21,7 +21,7 @@ interface TransactionContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionContextData>({} as  TransactionContextData)
+const TransactionsContext = createContext<TransactionContextData>({} as  TransactionContextData)
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -49,4 +49,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext)
+
+  return context
 }
